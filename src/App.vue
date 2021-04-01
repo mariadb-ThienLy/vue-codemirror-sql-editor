6209562823e13043d0b9cdfb7b0e897d2f29e33c
Thien Ly <thien.ly@mariadb.com>
2021-04-01 18:03:07 +0300

    Prototyping sql editor with code-mirror
    This creates simple editor view that proves the
    customize ability in terms of visual styling, syntax
    color highlighting, auto completion.
    
    To add reserved words of MariaDB, this requires
    creating custom sql hint parser.

--- src/App.vue
+++ src/App.vue
@@ -1,11 +1,14 @@
 <template>
     <div id="app">
-        <editor-view :dist="dist" />
+        <h1 class="title">Editor view using codemirror</h1>
+        <editor-view :dist="dist" @on-change="onEditorChange" />
+        <p>Query to sent via API</p>
+        <code>{{ queryData }}</code>
     </div>
 </template>
 
 <script>
-import EditorView from './components/EditorView.vue'
+import EditorView from './components/EditorVIew/index.vue'
 
 export default {
     name: 'App',
@@ -14,46 +17,18 @@ export default {
     },
     data() {
         return {
-            dist: [
-                { className: 'sql', text: 'SELECT' },
-                { className: 'sql', text: 'FROM' },
-                { className: 'sql', text: 'WHERE' },
-                { className: 'sql', text: 'INNER' },
-                { className: 'sql', text: 'JOIN' },
-                { className: 'sql', text: 'UNION' },
-                { className: 'sql', text: 'EXEC' },
-                { className: 'sql', text: 'INSERT' },
-                { className: 'sql', text: 'INTO' },
-                { className: 'sql', text: 'VALUES' },
-                { className: 'sql', text: 'UPDATE' },
-                { className: 'sql', text: 'DELETE' },
-                { className: 'sql', text: 'GROUP' },
-                { className: 'sql', text: 'BY' },
-                { className: 'sql', text: 'HAVING' },
-                { className: 'sql', text: 'IS' },
-                { className: 'sql', text: 'DISTINCT' },
-                { className: 'sql', text: 'OUTER' },
-                { className: 'sql', text: 'TOP' },
-                { className: 'sql', text: 'EXISTS' },
-                { className: 'sql', text: 'WHEN' },
-                { className: 'sql', text: 'CASE' },
-                { className: 'sql', text: 'CAST' },
-                { className: 'sql', text: 'IN' },
-                { className: 'sql', text: 'NULL' },
-                { className: 'table', text: 'te_cash_exchange_new' },
-                { className: 'table', text: 'ParamsAtos' },
-                { className: 'table', text: 'te_client_transfers' },
-                { className: 'column', text: 'status_cash' },
-                { className: 'column', text: 'datet' },
-                { className: 'column', text: 'ammount' },
-                { className: 'column', text: 'cash_exchange_id_start' },
-                { className: 'column', text: 'cash_exchange_id_end' },
-                { className: 'pf', text: 'AddParamAtos' },
-                { className: 'pf', text: 'verify_backoffice_user' },
-                { className: 'pf', text: 'checkAllowOperation' },
-                { className: 'pf', text: 'addMoneyIn_01' },
-            ],
+            dist: {
+                table1: ['col_A', 'col_B', 'col_C'],
+                table2: ['other_columns1', 'other_columns2'],
+            },
+            queryData: null,
         }
+    },
+
+    methods: {
+        onEditorChange(v) {
+            this.queryData = v
+        },
     },
 }
 </script>
@@ -63,8 +38,9 @@ export default {
     font-family: Avenir, Helvetica, Arial, sans-serif;
     -webkit-font-smoothing: antialiased;
     -moz-osx-font-smoothing: grayscale;
+    margin: 60px;
+}
+.title {
     text-align: center;
-    color: #2c3e50;
-    margin-top: 60px;
 }
 </style>
---
+++ src/components/EditorVIew/index.vue
@@ -0,0 +1,54 @@
+<template>
+    <textarea ref="txt" class="txt" />
+</template>
+
+<script>
+import CodeMirror from 'codemirror'
+import 'codemirror/lib/codemirror.css'
+import './mariadb-style.scss'
+import 'codemirror/addon/hint/show-hint.js'
+// For prototyping purpose, using readymade sql hint parser
+import 'codemirror/addon/hint/sql-hint.js'
+export default {
+    name: 'editor-view',
+    props: {
+        dist: { type: Object, require: true },
+    },
+    data() {
+        return {
+            queryData: null,
+            editor: null,
+        }
+    },
+    watch: {
+        queryData: function (v) {
+            this.$emit('on-change', v)
+        },
+    },
+    mounted() {
+        this.editor = CodeMirror.fromTextArea(this.$refs.txt, {
+            tabSize: 4,
+            mode: 'text/x-mariadb',
+            theme: 'mariadb-style',
+            lineNumbers: true,
+            line: true,
+            lineWrapping: true,
+            hintOptions: {
+                completeSingle: false,
+                hint: CodeMirror.hint.sql,
+                tables: this.dist,
+            },
+            extraKeys: { 'Ctrl-Space': 'autocomplete' },
+        })
+
+        this.editor.on('keypress', (editor) => {
+            editor.showHint()
+        })
+
+        const self = this
+        this.editor.on('change', function (cm) {
+            self.queryData = cm.getValue()
+        })
+    },
+}
+</script>
