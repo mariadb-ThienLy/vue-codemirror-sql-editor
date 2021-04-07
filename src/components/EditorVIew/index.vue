f46ac05632ff508d02aa824db3b621b1445aee2a
Thien Ly <thien.ly@mariadb.com>
2021-04-07 16:19:06 +0300

    Add searching addon

--- src/components/EditorVIew/index.vue
+++ src/components/EditorVIew/index.vue
@@ -4,11 +4,19 @@
 
 <script>
 import CodeMirror from 'codemirror'
+import { format } from 'sql-formatter'
 import 'codemirror/lib/codemirror.css'
 import './mariadb-style.scss'
 import 'codemirror/addon/hint/show-hint.js'
-// For prototyping purpose, using readymade sql hint parser
 import 'codemirror/addon/hint/sql-hint.js'
+import 'codemirror/addon/search/search.js'
+import 'codemirror/addon/search/matchesonscrollbar.js'
+import 'codemirror/addon/search/matchesonscrollbar.css'
+import 'codemirror/addon/search/match-highlighter.js'
+import 'codemirror/addon/search/jump-to-line.js'
+import 'codemirror/addon/dialog/dialog.js'
+import 'codemirror/addon/dialog/dialog.css'
+
 export default {
     name: 'editor-view',
     props: {
@@ -29,22 +37,34 @@ export default {
             lineNumbers: true,
             line: true,
             lineWrapping: true,
+            hint: CodeMirror.hint.sql,
             hintOptions: {
                 completeSingle: false,
-                hint: CodeMirror.hint.sql,
                 tables: this.dist,
             },
-            extraKeys: { 'Ctrl-Space': 'autocomplete' },
+            extraKeys: { 'Ctrl-Space': 'autocomplete', 'Ctrl-Shift-I': this.autoFormatSelection },
         })
 
-        this.editor.on('keypress', (editor) => {
-            editor.showHint()
-        })
+        this.editor.on('keypress', (editor) => editor.showHint())
 
         const self = this
-        this.editor.on('change', function (cm) {
-            self.$emit('on-change', cm.getValue())
-        })
+        this.editor.on('change', (cm) => self.$emit('on-change', cm.getValue()))
+    },
+    methods: {
+        mariadbFormatter(v) {
+            return format(v, {
+                language: 'mariadb',
+                indent: '    ',
+                uppercase: true,
+                linesBetweenQueries: 1,
+            })
+        },
+        autoFormatSelection() {
+            let sqlcontent = ''
+            sqlcontent = this.editor.getValue()
+            sqlcontent = this.mariadbFormatter(sqlcontent)
+            this.editor.getDoc().setValue(sqlcontent)
+        },
     },
 }
 </script>
